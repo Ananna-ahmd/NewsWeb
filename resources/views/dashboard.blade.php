39457da0fdff4d1d43a47faea72218b20ca93f17
Ananna-ahmd <anuprof29@gmail.com>
2025-01-05 12:26:06 +0600

    Add sidebar

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,9 +8,21 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
+            <div class="sidebar w-64 bg-yellow-500 text-black h-screen p-4">
+    <a class="block py-2 px-4 rounded-lg text-lg font-semibold hover:bg-blue-700 active:bg-blue-800" href="">
+        Homepage
+    </a>
+    <a class="block py-2 px-4 rounded-lg text-lg font-semibold hover:bg-blue-700 active:bg-blue-800" href="">
+         Create News
+    </a>
+    <a class="block py-2 px-4 rounded-lg text-lg font-semibold hover:bg-blue-700 active:bg-blue-800" href="#contact">
+        Create Category
+    </a>
+    <a class="block py-2 px-4 rounded-lg text-lg font-semibold hover:bg-blue-700 active:bg-blue-800" href="#about">
+        Update
+    </a>
+</div>
+
             </div>
         </div>
     </div>
